aa743981c6c458383485062159e7c0699a1964b4
Öktem Kalkan <oektem-kalkan@t-online.de>
2023-06-19 10:41:45 +0200

    input-test

--- components/Input/Input.test.js
+++ components/Input/Input.test.js
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+    render(<label></label>)
+});
 
 test("calls callback on every user input", async () => {});
